0b5847618b25c461dc89fe4c439d3b750aa2c71b
djrloria <djrloria@gmail.com>
2017-05-12 20:41:32 -0500

    initial commit. create core codes for the operations. added txt files for liri app as well as for log.

---
+++ key.js
@@ -0,0 +1,8 @@
+//console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'jhYtZzrMUr5c04JimCEyYf94A',
+  consumer_secret: 'JWDz3810yIycWbOgSgvRedq94tmpmBPY4Xb0keALz1wsKLZQtE',
+  access_token_key: '862082045018877952-1YcCpkxUZHp56NtFZe3Nt1jg1CpmI6T',
+  access_token_secret: 'iqZBojGSNGgRkkK8jy8B7HuVbCDee2iGGgHs4vFPyNZNJ',
+}
